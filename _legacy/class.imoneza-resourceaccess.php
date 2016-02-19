18fa4742a491d2fe2c2f34eb7d8ee4210816dda0
Aaron Saray <me@aaronsaray.com>
2016-02-19 15:43:49 -0600

    starting to manage legacy code.

---
+++ _legacy/class.imoneza-resourceaccess.php
@@ -0,0 +1,118 @@
+<?php
+    
+class iMoneza_ResourceAccess extends iMoneza_API {
+
+    protected $cookieExpiration;
+
+    public function __construct()
+    {
+        $options = get_option('imoneza_options');
+        parent::__construct($options, $options['ra_api_key_access'], $options['ra_api_key_secret'], IMONEZA__RA_API_URL);
+
+        // 14 days
+        $this->cookieExpiration = 60 * 60 * 24 * 14;
+    }
+
+    public function getResourceAccess($externalKey, $resourceURL)
+    {
+        try {
+            $userToken = '';
+
+            // Check for excluded user agents
+            if (isset($this->options['access_control_excluded_user_agents']) && $this->options['access_control_excluded_user_agents'] != '') {
+                foreach (explode("\n", $this->options['access_control_excluded_user_agents']) as $userAgent) {
+                    if ($userAgent == $_SERVER['HTTP_USER_AGENT'])
+                        return;
+                }
+            }
+
+            if (isset($_REQUEST['iMonezaTUT'])) {
+                // The user just authenticated at iMoneza, and iMoneza is sending the temporary user token back to us
+                $temporaryUserToken = $_REQUEST['iMonezaTUT'];
+                $resourceAccessData = $this->getResourceAccessDataFromTemporaryUserToken($externalKey, $resourceURL, $temporaryUserToken);
+            } else {
+                $userToken = $_COOKIE['iMonezaUT'];
+                $resourceAccessData = $this->getResourceAccessDataFromExternalKey($externalKey, $resourceURL, $userToken);
+            }
+
+            $userToken = $resourceAccessData['UserToken'];
+            setcookie('iMonezaUT', $userToken, time() + $this->cookieExpiration, '/');
+
+            // Prevent major caching plugins (WP Super Cache, W3 Total Cache) from caching the page if it's an iMoneza-managed resource
+            if ($resourceAccessData['AccessReason'] == 'Deny' || $resourceAccessData['AccessReason'] == 'Quota' || $resourceAccessData['AccessReason'] == 'Subscription' || $resourceAccessData['AccessReason'] == 'Purchase' || $resourceAccessData['AccessReason'] == 'Free' || $resourceAccessData['AccessReason'] == 'PropertyUser') {
+                define('DONOTCACHEPAGE', TRUE);
+            }
+
+            if ($resourceAccessData['AccessActionURL'] && strlen($resourceAccessData['AccessActionURL']) > 0)
+            {
+                $url = $resourceAccessData['AccessActionURL'];
+                $url = $url . '&OriginalURL=' . rawurlencode($resourceURL);
+                wp_redirect($url);
+                exit;
+            }
+        } catch (Exception $e) {
+            // Default to open access if there's some sort of exception
+            if (IMONEZA__DEBUG)
+                throw $e;
+        }
+    }
+
+    public function getResourceAccessDataFromExternalKey($externalKey, $resourceURL, $userToken) {
+        $request = new iMoneza_RestfulRequest($this);
+        $request->method = 'GET';
+        $request->uri = '/api/Resource/' . $this->accessKey . '/' . $externalKey;
+        $request->getParameters['ResourceURL'] = $resourceURL;
+        $request->getParameters['UserToken'] = $userToken;
+        $request->getParameters['IP'] = $this->getClientIP();
+
+        $response = $request->getResponse();
+
+        if ($response['response']['code'] == '404') {
+            throw new Exception('An error occurred with the Resource Access API key. Make sure you have valid Access Management API keys set in the iMoneza plugin settings.');
+        } else {
+            return json_decode($response['body'], true);
+        }
+    }
+
+    public function getResourceAccessDataFromTemporaryUserToken($externalKey, $resourceURL, $temporaryUserToken) {
+        $request = new iMoneza_RestfulRequest($this);
+        $request->method = 'GET';
+        $request->uri = '/api/TemporaryUserToken/' . $this->accessKey . '/' . $temporaryUserToken;
+        $request->getParameters['ResourceKey'] = $externalKey;
+        $request->getParameters['ResourceURL'] = $resourceURL;
+        $request->getParameters['IP'] = $this->getClientIP();
+
+        $response = $request->getResponse();
+
+        if ($response['response']['code'] == '404') {
+            throw new Exception('An error occurred with the Resource Access API key. Make sure you have valid Access Management API keys set in the iMoneza plugin settings.');
+        } else {
+            return json_decode($response['body'], true);
+        }
+    }
+
+    private function getClientIP() {
+        $ipAddress = '';
+        if (getenv('HTTP_CLIENT_IP'))
+            $ipAddress = getenv('HTTP_CLIENT_IP');
+        else if (getenv('HTTP_X_FORWARDED_FOR'))
+            $ipAddress = getenv('HTTP_X_FORWARDED_FOR');
+        else if (getenv('HTTP_X_FORWARDED'))
+            $ipAddress = getenv('HTTP_X_FORWARDED');
+        else if (getenv('HTTP_FORWARDED_FOR'))
+            $ipAddress = getenv('HTTP_FORWARDED_FOR');
+        else if (getenv('HTTP_FORWARDED'))
+           $ipAddress = getenv('HTTP_FORWARDED');
+        else if (getenv('REMOTE_ADDR'))
+            $ipAddress = getenv('REMOTE_ADDR');
+
+        // Strip out the port number on an IPv4 address
+        if (substr_count($ipAddress, '.') == 3 && strpos($ipAddress, ':') > 0) {
+            $parts = explode(':', $ipAddress, 2);
+            $ipAddress = $parts[0];
+        }
+            
+        return $ipAddress;
+    }
+}
+?>
---
+++ _legacy/class.imoneza.php
@@ -0,0 +1,131 @@
+<?php
+    
+class iMoneza {
+	private $options;
+
+    public function __construct()
+    {
+        $this->options = get_option('imoneza_options');
+
+        // If there's an Access API access key, and we're using client-side access control, create the JavaScript snippet
+        if (isset($this->options['ra_api_key_access']) && $this->options['ra_api_key_access'] != '' && (!isset($this->options['access_control']) || $this->options['access_control'] == 'JS')) {
+            add_action('wp_head', array($this, 'create_snippet'));
+        }
+
+        // If 'no_dynamic' isn't set, then make sure we add the dynamic resource creation block to every page
+        if (!isset($this->options['no_dynamic']) || $this->options['no_dynamic'] != '1') {
+            add_action('wp_head', array($this, 'create_dynamic'));
+        }
+
+        // Perform server-side access control
+        if (isset($this->options['ra_api_key_secret']) && $this->options['ra_api_key_secret'] != '' && isset($this->options['access_control']) && $this->options['access_control'] == 'SS') {
+            add_action('template_redirect', array($this, 'imoneza_template_redirect'));
+            add_action('wp_head', array($this, 'create_reference'));
+        }
+    }
+
+    public function imoneza_template_redirect()
+    {
+        $resourceValues = $this->get_resource_values();
+        if ($resourceValues['key'] == '')
+            return;
+
+        $resourceAccess = new iMoneza_ResourceAccess();
+        $response = $resourceAccess->getResourceAccess($resourceValues['key'], $resourceValues['url']);
+    }
+
+    private function get_resource_values()
+    {
+        $values = array();
+        $values['key'] = '';
+        $values['name'] = '';
+        $values['title'] = '';
+        $values['description'] = '';
+        $values['publicationDate'] = '';
+        $values['url'] = '';
+        
+        if (is_page() || is_single()) {
+            $this_post = get_post();
+
+            $values['key'] = $this_post->ID;
+            $values['name'] = $this_post->post_title;
+            $values['title'] = $this_post->post_title;
+            $values['description'] = $this_post->post_excerpt;
+            $values['publicationDate'] = $this_post->post_date;
+            $values['url'] = get_permalink($this_post->ID);
+        } else if (is_category()) {
+            $cat = get_query_var('cat');
+            $this_category = get_category($cat);
+
+            $values['key'] = 'Category-' . $this_category->cat_ID;
+            $values['name'] = $this_category->cat_name;
+            $values['title'] = $this_category->cat_name;
+            $values['url'] = get_category_link($this_category->cat_ID);
+        } else if (is_front_page()) {
+            $values['key'] = 'FrontPage';
+            $values['name'] = 'Front Page';
+            $values['title'] = 'Front Page';
+            $values['url'] = get_home_url();
+        } else if (is_tag()) {
+            $tag = get_query_var('tag');
+            $this_tag = get_term_by('name', $tag, 'post_tag');
+
+            $values['key'] = 'Tag-' . $this_tag->term_id;
+            $values['name'] = $this_tag->name;
+            $values['title'] = $this_tag->name;
+            $values['description'] = $this_tag->description;
+            $values['url'] = get_term_link($this_tag->term_id);
+        }
+
+        // Ignore archive pages
+        // Ignore feeds
+
+        return $values;
+    }
+
+    // Adds the iMoneza JavaScript snippet to the HTML head of a page
+    public function create_snippet()
+    {
+        $public_api_key = $this->options['ra_api_key_access'];
+        $resourceValues = $this->get_resource_values();
+
+        if ($resourceValues['key'] != '') {
+            echo '
+                <script src="' . IMONEZA__RA_UI_URL . '/assets/imoneza.js"></script>
+                <script type="text/javascript">
+                    iMoneza.ResourceAccess.init({
+                        ApiKey: "' . $public_api_key . '",
+                        ResourceKey: "' . $resourceValues['key'] . '"
+                    });
+                </script>
+            ';
+        }
+    }
+
+    // Adds the iMoneza JavaScript reference to the HTML head of a page
+    public function create_reference()
+    {
+        echo '<script src="' . IMONEZA__RA_UI_URL . '/assets/imoneza.js"></script>';
+    }
+
+    // Adds the dynamic resource creation block to the HTML head of a page
+    public function create_dynamic()
+    {
+        $resourceValues = $this->get_resource_values();
+
+        if ($resourceValues['key'] != '') {
+            echo '
+                <script type="application/imoneza">
+                    <Resource>
+                        <Name>' . $resourceValues['name'] . '</Name>
+                        <Title>' . $resourceValues['title'] . '</Title>' .
+                        ($resourceValues['description'] == '' ? '' :  '<Description>' . $resourceValues['description'] . '</Description>') . 
+                        ($resourceValues['publicationDate'] == '' ? '' : '<PublicationDate>' . $resourceValues['publicationDate'] . '</PublicationDate>') .
+                    '</Resource>
+                </script>
+            ';
+        }
+    }
+}
+
+?>
