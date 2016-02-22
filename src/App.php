f6216810ea4712f55f9500296a2308ad56c232a9
Aaron Saray <me@aaronsaray.com>
2016-02-22 13:30:33 -0600

    updated the code to be better organized on the app level.

--- src/App.php
+++ src/App.php
@@ -40,48 +40,9 @@ class App
     public function __invoke()
     {
         $di = $this->di;
-
         $firstTime = empty($this->getOptions());
 
-        if ($firstTime) {
-            $this->addAdminNoticeConfigNeeded();
-        }
-
-        add_action('admin_init', function() {
-            register_setting(self::$optionsKey, self::$optionsKey);
-        });
-
-        add_action('admin_menu', function() use ($firstTime, $di) {
-            add_options_page('iMoneza Options', 'iMoneza', 'manage_options', self::SETTINGS_PAGE_IDENTIFIER, $firstTime ? $di['controller.first-time-options'] : $di['controller.options']);
-        });
-
-        add_action('wp_ajax_first-time-settings', function() use ($di) {
-            /** @var \iMonezaPRO\Controller\FirstTimeOptions $controller */
-            $controller = $di['controller.first-time-options'];
-            $controller();
-        });
-        add_action('wp_ajax_settings', function() use ($di) {
-            /** @var \iMonezaPRO\Controller\Options $controller */
-            $controller = $di['controller.options'];
-            $controller();
-        });
-        add_action('wp_ajax_refresh_settings', function() use ($di) {
-            /** @var \iMonezaPRO\Controller\RefreshOptions $controller */
-            $controller = $di['controller.refresh-options'];
-            $controller();
-        });
-
-        add_action('admin_enqueue_scripts', function() {
-            wp_register_style('imoneza-admin-css', WP_PLUGIN_URL . '/imoneza-pro/assets/css/admin.css');
-            wp_enqueue_style('imoneza-admin-css');
-            wp_enqueue_script('jquery');
-            wp_enqueue_script('jquery-form');
-            wp_enqueue_script('imoneza-admin-js', WP_PLUGIN_URL . '/imoneza-pro/assets/js/admin.js', [], false, true);
-        });
-
-
         // this is scheduled hourly AFTER the first time we've kicked this off, or if we have this configured
-        $di = $this->di;
         add_action('imoneza_hourly', function() use ($di) {
             /** @var \iMonezaPRO\Controller\RefreshOptions $controller */
             $controller = $di['controller.refresh-options'];
@@ -94,6 +55,44 @@ class App
         register_deactivation_hook('imoneza-pro/imoneza-pro.php', function() {
             wp_clear_scheduled_hook('imoneza_hourly');
         });
+
+        if ($firstTime) {
+            $this->addAdminNoticeConfigNeeded();
+        }
+
+        if (is_admin()) {
+            add_action('admin_init', function () {
+                register_setting(self::$optionsKey, self::$optionsKey);
+            });
+
+            add_action('admin_menu', function () use ($firstTime, $di) {
+                add_options_page('iMoneza Options', 'iMoneza', 'manage_options', self::SETTINGS_PAGE_IDENTIFIER, $firstTime ? $di['controller.first-time-options'] : $di['controller.options']);
+            });
+
+            add_action('wp_ajax_first-time-settings', function () use ($di) {
+                /** @var \iMonezaPRO\Controller\FirstTimeOptions $controller */
+                $controller = $di['controller.first-time-options'];
+                $controller();
+            });
+            add_action('wp_ajax_settings', function () use ($di) {
+                /** @var \iMonezaPRO\Controller\Options $controller */
+                $controller = $di['controller.options'];
+                $controller();
+            });
+            add_action('wp_ajax_refresh_settings', function () use ($di) {
+                /** @var \iMonezaPRO\Controller\RefreshOptions $controller */
+                $controller = $di['controller.refresh-options'];
+                $controller();
+            });
+
+            add_action('admin_enqueue_scripts', function () {
+                wp_register_style('imoneza-admin-css', WP_PLUGIN_URL . '/imoneza-pro/assets/css/admin.css');
+                wp_enqueue_style('imoneza-admin-css');
+                wp_enqueue_script('jquery');
+                wp_enqueue_script('jquery-form');
+                wp_enqueue_script('imoneza-admin-js', WP_PLUGIN_URL . '/imoneza-pro/assets/js/admin.js', [], false, true);
+            });
+        }
     }
 
     /**
