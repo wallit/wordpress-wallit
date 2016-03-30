c0fe714a5b1f72d26bd7084cdec62202ea257849
Aaron Saray <me@aaronsaray.com>
2016-03-30 13:22:55 -0500

    updated comments for better attribution.

--- imoneza-pro.php
+++ imoneza-pro.php
@@ -1,11 +1,13 @@
 <?php
-/*
-Plugin Name: iMoneza PRO
-Plugin URI: https://github.com/iMoneza/wordpress-imoneza-pro
-Description: This plugin lets you provide paid access control mechanisms on your WordPress by using iMoneza.
-Author: iMoneza
-Version: 2.0.0
-*/
+/**
+ * Plugin Name: iMoneza PRO
+ * Plugin URI: https://github.com/iMoneza/wordpress-imoneza-pro
+ * Description: This plugin lets you provide paid access control mechanisms on your WordPress by using iMoneza.
+ * Author: iMoneza
+ * Author URI: http://imoneza.com
+ * Version: 2.0.0
+ * License: GPLv3
+ */
 
 if (!defined('ABSPATH')) {
 	header('HTTP/1.0 403 Forbidden');
